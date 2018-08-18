4642bbfdf20e6d818a7a076522d7c65fe4b30d9d
linux5396 <929159338@qq.com>
2018-08-18 22:36:29 +0800

    添加了成员信息类，创建了信息类mapper

---
+++ src/main/java/com/qg/www/dao/UserInfoDao.java
@@ -0,0 +1,9 @@
+package com.qg.www.dao;
+
+/**
+ * @author net
+ * @version 1.0
+ * 成员信息dao接口
+ */
+public interface UserInfoDao {
+}
--- src/main/java/com/qg/www/models/UserInfo.java
+++ src/main/java/com/qg/www/models/UserInfo.java
@@ -1,5 +1,6 @@
 package com.qg.www.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,50 @@ import org.springframework.stereotype.Service;
 @Service
 @Setter
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserInfo {
-
+    /**
+     * 用户信息编号
+     */
+    private Integer userInfoId;
+    /**
+     * 成员名字
+     */
+    private String name;
+    /**
+     * 组别
+     */
+    private String group;
+    /**
+     * 学院
+     */
+    private String college;
+    /**
+     * 学号
+     */
+    private String studentNum;
+    /**
+     * 电话号码
+     */
+    private String tel;
+    /**
+     * 籍贯
+     */
+    private String birthplace;
+    /**
+     * QQ
+     */
+    private String qq;
+    /**
+     * 邮件
+     */
+    private String email;
+    /**
+     * 简介
+     */
+    private String description;
+    /**
+     * 头像URL
+     */
+    private String url;
 }
